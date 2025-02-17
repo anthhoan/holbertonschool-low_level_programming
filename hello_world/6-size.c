b9eca417ca27d5059a550b823b3d47d44dee46e2
anthhoan <anthhoan@gmail.com>
2025-02-17 07:34:54 +0000

    a

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-printf("Size of char: %lu bytes\n", sizeof(unsigned char));
+printf("Size of a char: %lu bytes\n", sizeof(unsigned char));
 printf("Size of an int: %lu bytes\n", sizeof(unsigned int));
 printf("Size of a long int: %lu bytes\n", sizeof(unsigned long int));
 printf("Size of a long long int: %lu bytes\n", sizeof(unsigned long long int));
