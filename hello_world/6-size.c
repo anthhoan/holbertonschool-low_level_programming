2869715e4f057142275fd2e61335bdbf980e2bdf
anthhoan <anthhoan@gmail.com>
2025-02-17 07:35:49 +0000

    brackets

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu bytes\n", sizeof(unsigned char));
-printf("Size of an int: %lu bytes\n", sizeof(unsigned int));
-printf("Size of a long int: %lu bytes\n", sizeof(unsigned long int));
-printf("Size of a long long int: %lu bytes\n", sizeof(unsigned long long int));
-printf("Size of a float: %lu bytes\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(unsigned char));
+printf("Size of an int: %lu byte(s)\n", sizeof(unsigned int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(unsigned long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(unsigned long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
