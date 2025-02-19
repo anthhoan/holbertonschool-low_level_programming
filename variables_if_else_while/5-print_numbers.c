6c6ce50b13801a1b5465bdf9f7466c92781a89a9
anthhoan <anthhoan@gmail.com>
2025-02-19 09:11:40 +0000

    counting up

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	return (0);
+}
