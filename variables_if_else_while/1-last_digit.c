b13042b2c3da12e5d4210c7ab78c51bf14c992ff
anthhoan <anthhoan@gmail.com>
2025-02-19 03:39:18 +0000

    and

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 
 	if (lastDigit == 0)
 	{
-		printf("Last digit of %d is %d is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
 
 	return (0);
