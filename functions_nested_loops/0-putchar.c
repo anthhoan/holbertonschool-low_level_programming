aa0bfec4709b158eae96b848aa7c769a2e81c9df
anthhoan <anthhoan@gmail.com>
2025-02-20 05:03:20 +0000

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char _putchar;
+
+	putchar(_putchar);
+	{
+		putchar('\n');
+		return (0);
+	}
+}
