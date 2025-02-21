16464b24b22c8e033832d7cb8431429521ed098e
anthhoan <anthhoan@gmail.com>
2025-02-21 00:53:25 +0000

    upload

---
+++ functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	char alphabet;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	}
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define ASDF
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
