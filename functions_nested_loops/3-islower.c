c97adf34a1477098ccfb8bfe6025bcd941bff6b0
anthhoan <anthhoan@gmail.com>
2025-02-21 04:44:16 +0000

    return 0 or 1

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 /**
- * islower.c - _putchar
- *
- * Return ???
+ * _islower - checks if a character is lowercase
+ * @c: character to be verified
+ * Return: returns a value of 0 or 1
  */
-
 int _islower(int c)
 {
-	
+	if(c <= 'a' && c >= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
