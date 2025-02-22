885eb0775216667694bf1883bcf6ea01e995f0b4
anthhoan <anthhoan@gmail.com>
2025-02-22 10:03:03 +0000

    reword

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_signL prints numbers
+ * print_signL prints numbers using _putchar
  * @n: The number to be checked
  * Return: will return + or -
  */
@@ -10,18 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 		{
-			putchar(43);
+			_putchar(43);
 			return (1);
 		}
-		else
-			if (n < 0)
+		else if (n < 0)
 			{
-				putchar(45);
+				_putchar(45);
 				return (-1);
 			}
 			else
 			{
-				putchar(48);
+				_putchar(48);
 				return (0);
 			}
 }
