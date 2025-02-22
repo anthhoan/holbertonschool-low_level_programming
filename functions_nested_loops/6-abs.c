09fa4db3109ff564e574b80a588780b0959e6d6c
anthhoan <anthhoan@gmail.com>
2025-02-22 11:47:08 +0000

    help :)

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: represents the abosulute value
+ * Return: the distance the value is from 0
+ */
+
+int _abs(int)
+{
+	int r;
+
+	abs_value =
