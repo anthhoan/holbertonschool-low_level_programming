4cf27a1148ac3229ce53c257206f7e12009a4571
anthhoan <anthhoan@gmail.com>
2025-02-23 02:50:52 +0000

    hours and minutes

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
- * Return: the time
+ * Return: nothing
  */
 
 void jack_bauer(void)
@@ -12,18 +12,21 @@ void jack_bauer(void)
 
 	for (hour = 48; hour <= 50; hour++)
 	{
-		for (hours = 48; hours <= 52; hours++)
+		for (hours = 48; hours <= 57; hours++)
 		{
-			for (minute = 48; minute <= 53; minute++)
+			if ((hour <= 49 && hours <= 57) || (hour <= 50 && hours <= 51))
 			{
-				for (minutes = 48; minutes <= 57; minutes++)
+				for (minute = 48; minute <= 53; minute++)
 				{
-					_putchar(hour);
-					_putchar(hours);
-					_putchar(58);
-					_putchar(minute);
-					_putchar(minutes);
-					_putchar('\n');
+					for (minutes = 48; minutes <= 57; minutes++)
+					{
+						_putchar(hour);
+						_putchar(hours);
+						_putchar(58);
+						_putchar(minute);
+						_putchar(minutes);
+						_putchar('\n');
+					}
 				}
 			}
 		}
