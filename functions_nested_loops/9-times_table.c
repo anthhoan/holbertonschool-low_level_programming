3ad5533a2f98db714b65b5f40e68fc276a806fe7
anthhoan <anthhoan@gmail.com>
2025-02-23 06:43:58 +0000

    9times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/** times_table - prints the times table
+ *
+ * Return: empty output
+ */
+
+void times_table(void)
+{
+	int firstDigit, secondDigit, result;
+
+	for (firstDigit = 0; firstDigit < 10; firstDigit++)
+	{
+		for (secondDigit = 0; secondDigit < 10; secondDigit++)
+		{
+			result = firstDigit * secondDigit;
+
+			if (secondDigit == 0)
+				_putchar('0' + result);
+
+			else if ( result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			if (secondDigit != 10)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
