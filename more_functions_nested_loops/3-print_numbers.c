a3bc46a408fd4297ffcd0071f9d3216186bf3806
anthhoan <anthhoan@gmail.com>
2025-02-24 00:23:08 +0000

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * Return: numbers
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	{
+		_putchar('\n');
+	}
+}
