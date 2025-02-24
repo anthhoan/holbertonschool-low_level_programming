e681abf4a7c831ec673055b46e3f13206c557d1b
anthhoan <anthhoan@gmail.com>
2025-02-24 01:40:06 +0000

    0to14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - call the more_numbers function
+ * Description: Print the numbers 0 to 14 ten times
+ * Return: 0 to 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int r, n;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 10)
+				_putchar('0' + n / 10);
+			_putchar('0' + n % 10);
+		}
+	_putchar('\n');
+	}
+}
