9456d24286bf30263d09a9a26f40828ee39e25a3
anthhoan <anthhoan@gmail.com>
2025-02-24 22:32:37 +0000

    diagonals

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,24 +10,21 @@ void print_diagonal(int n)
 	int row = 0;
 	int space;
 
+	while (row < n)
+	{
+		space = 0;
+
+		while (space < row)
+		{
+			_putchar(' ');
+			space++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		row++;
+	}
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
-	{
-		while (row <= n)
-		{
-			space = 0;
-
-			while (space <= row)
-			{
-				_putchar(' ');
-				space++;
-			}
-			_putchar('\\');
-			_putchar('\n');
-			row++;
-		}
 	}
 }
