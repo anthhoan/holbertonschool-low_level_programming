1a1a057a4c0162eb54b949d3d0d35cb2a0364aed
anthhoan <anthhoan@gmail.com>
2025-02-25 01:47:28 +0000

    hashtag

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/** print_square - prints a hashtag
+ *
+ * return: returns rows and hastags
+ */
+
+void print_square(int size)
+{
+	int row = 0;
+	int hash;
+
+	while (row < size)
+	{
+		hash = 0;
+
+		while (hash < size)
+		{
+			_putchar(35);
+			hash++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
