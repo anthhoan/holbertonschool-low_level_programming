c98ba5f9c4ebcf2bfb4a33dc1fa15b9bf43b7ebe
anthhoan <anthhoan@gmail.com>
2025-02-26 09:47:34 +0000

    _putchar

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - calls upon the _puts function
+ * @str: holds the string of text
+ * return: the string of text
+ */
+
+void _puts(char *str)
+{
+	int string;
+
+	while (str != string)
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar('\n');
+}
