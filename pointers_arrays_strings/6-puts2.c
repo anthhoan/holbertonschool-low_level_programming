33760bc304f83d3f5d34f4cfe4efc37fcab3268c
anthhoan <anthhoan@gmail.com>
2025-02-27 11:04:10 +0000

    += 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - calls upon the puts2 function
+ * @str: holds the string
+ * return: void
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
