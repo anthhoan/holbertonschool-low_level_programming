e6e02d40916fa1903bebe59153e0d2becb9d64d2
anthhoan <anthhoan@gmail.com>
2025-02-27 07:23:47 +0000

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - calls upon the print_rev function
+ * @s: holds the string of text
+ * return: void
+ */
+
+void print_rev(char *s)
+{
+	int stringLgnth = 0;
+
+	while (s[stringLgnth] != '\0')
+	{
+		stringLgnth++;
+	}
+
+	while (stringLgnth >= 0)
+	{
+		_putchar(s[stringLgnth]);
+		stringLgnth--;
+	}
+	_putchar('\n');
+}
