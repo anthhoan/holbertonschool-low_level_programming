22831f457db05322337e28322768be5b0a0b8c70
anthhoan <anthhoan@gmail.com>
2025-02-28 00:22:19 +0000

    print every other

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 {
 	int count = 0;
 
-	while (*str != '\0')
+	while (str[count] != '\0')
 	{
-		if (str[count] % 2 == 0)
+		if (count % 2 == 0)
 		_putchar(str[count]);
-		str++;
+		count++;
 	}
 	_putchar('\n');
 }
