26584b75ed0157f8ec9bb9b940ac8bf5cb344480
anthhoan <anthhoan@gmail.com>
2025-03-01 03:50:02 +0000

    copy the string, then write it into another variable

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - calls upon the strcpy function
+ * @src: stores the string we need to copy
+ * @dest: takes the string from src and prints
+ * return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while(*src != '\0')
+	{
+		*start = *src;
+		start++;
+		src++;
+	}
+	*start = '\0';
+	return dest;
+}
