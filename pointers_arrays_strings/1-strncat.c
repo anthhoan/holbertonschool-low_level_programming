2a3b6d16731a524b80c8a66549cba399e94e95cb
anthhoan <anthhoan@gmail.com>
2025-03-03 10:09:02 +0000

    strncpy

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
- * *_strncat - similar to strcat except that, it will use at most n bytes from src and src does not need to be null if n contains more bytes
- * @dest: source of 1st string
- * @src: source of 2nd string
- * @n: contains bytes from src
- * return: dest
+ * _strncat - concatenates two strings up to a specified number of bytes.
+ * @dest: Pointer to the destination string buffer
+ * @src: Pointer to the source string buffer
+ * @n: maximum number of bytes to be appended from @src
+ * Description:
+ * Step 1: Count the length of dest
+ * Step 2: Use a loop to copy characters from src to dest
+ * (up to n, not exceeding src length)
+ * Step 3: Be sure to null-terminate dest at the new length
+ * Return: Pointer to the resulting string of dest
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -26,9 +31,3 @@ char *_strncat(char *dest, char *src, int n)
 	dest[destCount] = '\0';
 	return (dest);
 }
-
-/**:
- * Step 1: Count the length of dest
- * Step 2: Use a loop to copy characters from src to dest (up to n, not exceeding src length)
- * Step 3: Be sure to null-terminate dest at the new length
- */
