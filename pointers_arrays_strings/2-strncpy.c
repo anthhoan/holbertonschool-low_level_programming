26849ec5454c7a76bfac4c538f8e57e701282bdf
anthhoan <anthhoan@gmail.com>
2025-03-03 23:51:48 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -21,7 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 		destCount++;
 		srcCount++;
 	}
-	if (destCount < n && destCount != n)
+	if (destCount != n && destCount != n)
 	{
 		dest[destCount] = '\0';
 	}
