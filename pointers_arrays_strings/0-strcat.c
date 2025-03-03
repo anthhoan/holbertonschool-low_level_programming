c752d87fc1d369447d5ff09958058c12d33eef0c
anthhoan <anthhoan@gmail.com>
2025-03-03 09:12:24 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int count = 0;
-	int startPosition;
+	int nulCount = 0;
+	int sndArray = 0;
 
-	while (dest[count] != '\0')
-	{					/** dest[count] is not NULL increment count until it reaches NULL */
-		count++
+	while (dest[nulCount] != '\0')
+	{
+		nulCount++;
 	}
-
-	startPosition = count;			/** count is now NULL, which we will assign to another variable */
-
-	count = 0;				/** reset count to 0 */
-
-	while (dest[startPosition + count] 
+	
+	while (src[sndArray] != '\0')
+	{
+	dest[nulCount] = src[sndArray];
+	nulCount++;
+	sndArray++;
+	}
+	dest[nulCount] = '\0';
+	return (dest);
+}
