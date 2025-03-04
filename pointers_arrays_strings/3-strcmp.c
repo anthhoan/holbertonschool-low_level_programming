5e3763732c4cbba83b53c07fcd534c7cc59eb4c8
anthhoan <anthhoan@gmail.com>
2025-03-04 03:25:58 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -18,7 +18,12 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[wordCount1] != '\0' && s2[wordCount2] != '\0')
 	{
-		return (*s1 - *s2);
+		{
+			if (s1[wordCount1] != s2[wordCount2])
+			{
+				return (*s1 - *s2);
+			}
+		}
 		wordCount1++;
 		wordCount2++;
 	}
