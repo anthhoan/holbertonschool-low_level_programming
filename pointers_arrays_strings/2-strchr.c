eb207cb37bd4d47cb89f79c9a0805a1fdd2f4d8c
anthhoan <anthhoan@gmail.com>
2025-03-05 11:13:51 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string then prints out the remainder
+ * @s: a pointer ti the string to be searched
+ * @c: the character to be located
+ * Return: the character 'c' in the string or s if null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int array = 0;
+
+	while (s[array] != '\0')
+	{
+		if (s[array] == c)
+		{
+			return (s + array);
+		}
+		if (s[array] == '\0')
+		{
+			return (s);
+		}
+		array++;
+	}
+	return (s);
+}
