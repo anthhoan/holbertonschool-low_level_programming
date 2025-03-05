3c7cb5fe1bc62aeae0b5165e6d73cb302fe407a8
anthhoan <anthhoan@gmail.com>
2025-03-05 01:01:14 +0000

    1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @string: string
+ * Return: s
+ */
+
+char *leet(char *string)
+{
+	int count1 = 0;
+	int count2;
+	char alphabet[] = "aAeEoOtT;:";
+	char replaceWith[] = "4433007711";
+
+	while (string[count1] != '\0')
+	{
+		count2 = 0;
+
+		while (alphabet[count2] != '\0')
+		{
+			if (string[count1] == alphabet[count2])
+			{
+				string[count1] = replaceWith[count2];
+			}
+			count2++;
+		}
+		count1++;
+	}
+	return (string);
+}
