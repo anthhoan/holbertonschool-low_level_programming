291f58405340633e82dcdaeacd9f752bea23634a
anthhoan <anthhoan@gmail.com>
2025-03-05 00:22:05 +0000

    cap_string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,17 +3,20 @@
 /**
  * cap_string - capitalizes all words of a string
  * @s: stores the given string
- * return: char
+ * Return: s
  */
 
 char *cap_string(char *s)
 {
 	int count = 0;
 	int capitalise = 1;
-	
+
 	while (s[count] != '\0')
 	{
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n' || s[count] == ',' || s[count] == '.' || s[count] == '!' || s[count] == '?' || s[count] == '"' || s[count] == '(' || s[count] == ')' || s[count] == '{' || s[count] == '}' ||  s[count] == ';')
+		if (s[count] == ' ' || s[count] == '\n' || s[count] == '\t'
+		|| s[count] == ',' || s[count] == '!' || s[count] == '?'
+		|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+		|| s[count] == '{' || s[count] == '}' || s[count] == '.')
 		{
 			capitalise = 1;
 		}
