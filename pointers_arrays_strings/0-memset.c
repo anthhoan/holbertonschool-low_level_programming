5d9e06be6fce30e4fc3e4d37f07f959ba2033985
anthhoan <anthhoan@gmail.com>
2025-03-05 05:53:59 +0000

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that fills memory with a constant byte
+ * @s: a pointer to the block of memory to fill
+ * @b: second parameter of type integer and converts it to unsigned char
+ * before using it to full a block of memory
+ * @n: the number of bytes to be set to the value
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int array = 0;
+
+	if (n == 0)
+	{
+		return (s);
+	}
+
+	while (array < n)
+	{
+		s[array] = b;
+		array++;
+	}
+	return (s);
+}
