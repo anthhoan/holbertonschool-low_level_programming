9e764f8259a97d3e0a3e91357a60c29630517cac
anthhoan <anthhoan@gmail.com>
2025-03-06 07:41:08 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,6 +7,35 @@
  * Return: number of bytes
  */
 
-unsigned int_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int length = 0;
+	int sCount = 0;
+	int aCount = 0;
+	int match = 0;
 
+	while (s[sCount] != '\0')
+	{
+		aCount = 0;
+		match = 0;
+
+		while (accept[aCount] != '\0')
+		{
+			if (s[sCount] == accept[aCount])
+			{
+			match = 1;
+			length++;
+			}
+			aCount++;
+
+		}	
+		
+		if (match == 0)
+		{
+			return (length);
+		}
+
+		sCount++;
+	}
+	return (length);
+}
