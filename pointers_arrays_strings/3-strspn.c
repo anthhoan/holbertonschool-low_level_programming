1027b6bd7524542a3303d66ec0e75dd405bae74e
anthhoan <anthhoan@gmail.com>
2025-03-06 00:56:38 +0000

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: main string to be scanned
+ * @accept: string of characters to match s
+ * Return: number of bytes
+ */
+
+unsigned int_strspn(char *s, char *accept)
+{
+
