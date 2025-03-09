ce64429e817562a5178432730b5f769ab97bae69
anthhoan <anthhoan@gmail.com>
2025-03-09 07:10:12 +0000

    diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * @a: the pointer to the string of numbers
+ * @size:
+ * Return: void
+ */
+
+void print_diagsums(int *a, iint size)
+{
+
