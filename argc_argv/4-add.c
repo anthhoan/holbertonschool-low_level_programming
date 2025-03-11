7721dbfb4a65d51d6eeca9401c3be00a9490059d
anthhoan <anthhoan@gmail.com>
2025-03-11 09:49:23 +0000

    addition

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: the added value of numbers, 0 if no numbers passed throgh the program
+ * otherwise 1 if symbols that are not digits is passed through
+ */
+
+int main(int argc, char *argv[])
+{
+	int firstNumber = 0;
+
+	if (argc != 3)
+	{
+		printf("0\n");
+		return (0);
+	}
+	
+	if (argc < 48 || argc > 57)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	while (
+	
+	return (argc);
+}
