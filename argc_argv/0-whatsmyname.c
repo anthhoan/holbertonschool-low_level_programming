0b2bff2a4aed60274efffeabeeb1cfe187247474
anthhoan <anthhoan@gmail.com>
2025-03-11 08:44:57 +0000

    print program name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: void, not being used in this program
+ * @argv: argv0 being the program name
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
-	
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
