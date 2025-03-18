95fcfe8c583517a9b30bf8b9ccd6c47bfc2bed87
anthhoan <anthhoan@gmail.com>
2025-03-18 06:48:59 +0000

    malloc duplicated

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy of a string to the given parameter
+ * @str: the given string
+ * Return: the pointer of the string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *array;
+	int counter = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	array = malloc(sizeof(char) * (i + 1));
+	if (array == 0)
+	{
+	return (NULL);
+	}
+
+	for (counter = 0; counter < i; counter++)
+	{
+		array[counter] = str[counter];
+	}
+	return (array);
+}
