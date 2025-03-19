74616af58ef27e3c0ea5836bfbc50bf40aa473a6
anthhoan <anthhoan@gmail.com>
2025-03-19 02:56:34 +0000

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,41 +11,31 @@
 char *str_concat(char *s1, char *s2)
 {
 	int count = 0;
-        int length1 = 0;
- 	int length2 = 0;
+	int length1 = 0;
+	int length2 = 0;
 	char *result;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	else
 	{
 		for (count = 0; s1[count] != '\0'; count++)
-		{
 			length1++;
-		}
 	}
-	
+
 	count = 0;
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	else
 	{
 		for (count = 0; s2[count] != '\0'; count++)
-		{
 			length2++;
-		}
 	}
 
 	result = malloc(sizeof(char) * (length1 + length2 + 1));
 		if (result == NULL)
-		{
 			return (NULL);
-		}
 
 	count = 0;
 
@@ -53,7 +43,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		result[count] = s1[count];
 	}
-	
+
 	for (count = 0; count < length2; count++)
 	{
 		result[length1 + count] = s2[count];
