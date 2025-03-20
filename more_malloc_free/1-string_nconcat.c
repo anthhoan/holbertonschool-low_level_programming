c02d68568eb6ed2eb6cd14a7f02abc286f4ada11
anthhoan <anthhoan@gmail.com>
2025-03-20 03:05:46 +0000

    concat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: the first set of string
+ * @s2: the second set of string
+ * @n: unsigned int
+ * Return:
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int count = 0;
+	unsigned int length1 = 0;
+	unsigned int length2 = 0;
+	char *finalConcat;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (count = 0; s1[count] != '\0'; count++)
+	{
+		length1++;
+	}
+
+	count = 0;
+
+	for (count = 0; s2[count] != '\0'; count++)
+	{
+		length2++;
+	}
+
+	if (n > length2)
+	{
+		n = length2;
+	}
+
+	finalConcat = malloc(sizeof(int) * (length1 + n + 1));
+	if (finalConcat == NULL)
+	{
+		return (NULL);
+	}
+
+	count = 0;
+
+	for (count = 0; count < length1; count++)
+	{
+		finalConcat[count] = s1[count];
+	}
+
+	for (count = 0; count < n; count++)
+	{
+		finalConcat[length1 + count] = s2[count];
+	}
+	finalConcat[length1 + count] = '\0';
+	return (finalConcat);
+}
