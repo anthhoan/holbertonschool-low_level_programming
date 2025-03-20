d6f258add6b7578df45151297ec2c1eb15822835
anthhoan <anthhoan@gmail.com>
2025-03-20 02:08:28 +0000

    malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memeory using malloc
+ * @b: unsigned interger being inputed
+ * Return: the pointer to malloc
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *i;
+
+	i = malloc(b);
+
+	if (i == NULL)
+	{
+		exit (98);
+	}
+	return (i);
+}
