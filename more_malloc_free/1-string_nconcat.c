87f0a6d4558bc37837b6c62c53080e1fe1c359af
anthhoan <anthhoan@gmail.com>
2025-03-20 03:15:00 +0000

    concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -6,7 +6,7 @@
  * @s1: the first set of string
  * @s2: the second set of string
  * @n: unsigned int
- * Return:
+ * Return: finalConcat
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -17,30 +17,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *finalConcat;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	for (count = 0; s1[count] != '\0'; count++)
-	{
 		length1++;
-	}
 
 	count = 0;
 
 	for (count = 0; s2[count] != '\0'; count++)
-	{
 		length2++;
-	}
 
 	if (n > length2)
-	{
 		n = length2;
-	}
 
 	finalConcat = malloc(sizeof(int) * (length1 + n + 1));
 	if (finalConcat == NULL)
@@ -51,14 +41,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	count = 0;
 
 	for (count = 0; count < length1; count++)
-	{
 		finalConcat[count] = s1[count];
-	}
 
 	for (count = 0; count < n; count++)
-	{
 		finalConcat[length1 + count] = s2[count];
-	}
+
 	finalConcat[length1 + count] = '\0';
 	return (finalConcat);
 }
