ae5aea5fba2fca00efceb3f4fb9e0ca33e444ad3
anthhoan <anthhoan@gmail.com>
2025-03-22 02:09:25 +0000

    doggo

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Details of the dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
