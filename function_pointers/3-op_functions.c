7c13369978608887bf12496efa1a382119f209c0
anthhoan <anthhoan@gmail.com>
2025-03-25 05:18:19 +0000

    progress

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,27 +1,66 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * op_add - adds two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of both integers
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/** op_sub - subtracts two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of both integers
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - multiplies two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of both integers
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - divides two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of both integers, if b == 0 return error and exit 100
+ */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
+/** 
+ * op_mod - modulo of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of both integers, if b == 0 return error and exit 100
+ */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
