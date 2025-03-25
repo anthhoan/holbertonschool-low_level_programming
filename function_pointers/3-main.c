b61ea40ce5731958eeb48f7415c18095e9646395
anthhoan <anthhoan@gmail.com>
2025-03-25 06:45:30 +0000

    progress

---
+++ function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that calculates two given integers and the operator
+ * @argc: number of arguments including the program name
+ * @argv: the two number being printed and the operator
+ * Return: the result of the 2 integers being added, subtracted, multiplied
+ * divide or the remainder of the numbers divided.
+ * Number of arguments wrong (98)
+ * Operator is none of the above (99)
+ * Divides/Modulo by 0 (100)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	operator = get_op_func(argv[2]);
+	num2 = atoi(argv[3]);
+
+
