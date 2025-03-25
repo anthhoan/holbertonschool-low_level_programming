e369a9388cdab022956e65621b692245d75ebeb9
anthhoan <anthhoan@gmail.com>
2025-03-25 03:17:31 +0000

    int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,10 +10,18 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int counter = 0;
+
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	if (size <= 0)
 		return (-1);
 
-
+	for (counter = 0; counter < size; counter++)
+	{
+		if (cmp(array[counter]) != 0)
+			return (counter);
+	}
+	return (-1);
+}
