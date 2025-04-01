64a538edfb11b8498e6a8f36c3382796eb92041e
anthhoan <anthhoan@gmail.com>
2025-04-01 07:28:12 +0000

    separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: the string to be printed between numbers
+ * @n: unsigned int number
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	
