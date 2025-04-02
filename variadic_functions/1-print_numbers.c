7cc7e78a3dfe398b07fe8ef6a713feb3187b504e
anthhoan <anthhoan@gmail.com>
2025-04-02 03:06:10 +0000

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,6 +9,29 @@
  * Return: void
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	
+	/* always declare va_list */
+	va_list args;
+	unsigned int i = 0; /* counter to iterate */
+
+	va_start(args, n);
+	/* check if n is 0 */
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && separator != NULL)
+		{
+			printf("%s", separator); /* separator is a string */
+		}
+		printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+
+	va_end(args);
+}
