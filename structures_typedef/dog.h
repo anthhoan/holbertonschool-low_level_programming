94e6490019eeda6829c51b489422acbfc644b626
anthhoan <anthhoan@gmail.com>
2025-04-04 04:06:40 +0000

    struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - information about the dog
+ * @name: the name of the dog
+ * @age: how old the dog is
+ * @owner: who owns the dog
+ */
+
 typedef struct dog
 {
 	char *name;
